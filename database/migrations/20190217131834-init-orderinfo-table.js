9559db8f826ad238653e856b14b38983f4d96014
weixiang1999 <920666487@qq.com>
2019-02-18 11:39:58 +0800

    列名修改

--- database/migrations/20190217131834-init-orderinfo-table.js
+++ database/migrations/20190217131834-init-orderinfo-table.js
@@ -31,7 +31,7 @@ module.exports = {
       id: { type: INTEGER, primaryKey: true, autoIncrement: true },
       order_photo_id: INTEGER,
       order_type: ENUM('预定单', '正价', '套餐', '自来客'),
-      order_statu: ENUM('预定中', '待接受', '待跟进', '已跟进', '已完成', '已取消'),
+      order_status: ENUM('预定中', '待接受', '待跟进', '已跟进', '已完成', '已取消'),
       customer_name: STRING(20),
       customer_sex: ENUM('男', '女'),
       customer_phone: STRING(15),
